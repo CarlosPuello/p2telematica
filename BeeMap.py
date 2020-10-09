dcc9cc37ee3219cc405e3df4561b644dbf2a9bfe
Carlos <carlitosthb@hotmail.com>
2020-10-09 14:56:07 -0500

    poop

--- BeeMap.py
+++ BeeMap.py
@@ -1,24 +1,16 @@
 import math
 import itertools
+from operator import itemgetter
 from time import time
-
-class Abeja:
-    #Constructor
-    def __init__(self, latitud, longitud, altura, llave, peligro = False):
-        self.longitud = longitud
-        self.latitud = latitud
-        self.altura = altura
-        self.llave = llave
-        self.peligro = peligro
 
 class Colisiones:
     #constructor
-    def __init__(self, diccionarioAbejitas, mapGrid, contador = 0):
+    def __init__(self, listaAbejitas, mapGrid, contador = 0):
         self.contador = 0
-        self.diccionarioAbejitas = diccionarioAbejitas
+        self.listaAbejitas = listaAbejitas
         self.mapGrid = mapGrid
         self.mapa = None
-        if (len(diccionarioAbejitas) > 1):
+        if (len(listaAbejitas) > 1):
             self.hacerMapa()
         else:
             self.comparacionSimple()
@@ -26,50 +18,43 @@ class Colisiones:
         
     #Calcular distancia entre 2 abejas
     def calcularDistancia(self, abeja1, abeja2):
-        d = math.sqrt(((((abeja1.longitud-abeja2.longitud)*111111)**2)+(((abeja1.latitud-abeja2.latitud)*111111)**2)+((abeja1.altura-abeja2.altura)**2)))
+        d = math.sqrt(((((abeja1[1]-abeja2[1])*111111)**2)+(((abeja1[0]-abeja2[0])*111111)**2)+((abeja1[2]-abeja2[2])**2)))
         if d <= 100: #Si la diferencia de distancia es menor o igual a 100 metros, estan en peligro
-            if abeja1.peligro == False:
+            if abeja1[3] == False:
                 self.contador += 1
-            if abeja2.peligro == False:
+            if abeja2[3] == False:
                 self.contador +=1
-            abeja1.peligro = True
-            abeja2.peligro = True
+            abeja1[3] = True
+            abeja2[3] = True
 
     def hacerMapa(self):
         ini = time()
-        key_maxX = max(diccionarioAbejitas.keys(), key=(lambda k: diccionarioAbejitas[k].longitud))
-        key_minX = min(diccionarioAbejitas.keys(), key=(lambda k: diccionarioAbejitas[k].longitud))
-        key_maxY = max(diccionarioAbejitas.keys(), key=(lambda k: diccionarioAbejitas[k].latitud))
-        key_minY = min(diccionarioAbejitas.keys(), key=(lambda k: diccionarioAbejitas[k].latitud))
+        Xsorted = sorted(listaAbejitas, key=itemgetter(1))
+        Ysorted = sorted(listaAbejitas, key=itemgetter(0))
+        Xmax = Xsorted[-1][1]
+        Xmin = Xsorted[0][1]
+        Ymax = Ysorted[-1][0]
+        Ymin = Ysorted[0][0]
 
-        Xmax = diccionarioAbejitas[key_maxX].longitud
-        Xmin = diccionarioAbejitas[key_minX].longitud
-        Ymax = diccionarioAbejitas[key_maxY].latitud
-        Ymin = diccionarioAbejitas[key_minY].latitud
-        print('Valor maximo en y: ',Ymax)
-        print('Valor minimo en y: ',Ymin)
-        print('Valor maximo en x: ',Xmax)
-        print('Valor minimo en x: ',Xmin)
         ordenY = []
+        ordenX = []
         deltaY = abs(Ymax-Ymin)/self.mapGrid
+        deltaX = (abs(Xmax)-abs(Xmin))/self.mapGrid
         primerY = Ymax
+        primerX = Xmin
+
         for i in range(0,self.mapGrid+1):
             ordenY.append(primerY)
-            primerY -= deltaY
-
-        ordenX = []
-        deltaX = (abs(Xmax)-abs(Xmin))/self.mapGrid
-        primerX = Xmin
-        for i in range(0,self.mapGrid+1):
             ordenX.append(primerX)
+            primerY -= deltaY
             primerX -= deltaX
-        print ("Orden en x= "+ str(ordenX))
-        print ("Orden en y= "+ str(ordenY))
-
-        for i in range(len(diccionarioAbejitas)):
-            abeja = diccionarioAbejitas.get(i)
-            x = abeja.longitud
-            y = abeja.latitud
+            
+        relleno = time()
+        print("||| Haciendo el mapa sin rellenar: ", relleno-ini)
+        for i in range(len(listaAbejitas)):
+            abeja = listaAbejitas[i]
+            x = abeja[1]
+            y = abeja[0]
             j = self.mapGrid-1
             k = 0
             indiceX = 0
@@ -98,21 +83,23 @@ class Colisiones:
         self.mapa = mapa
 
         #print("el total de abejas es: "+str(contador))
-        print("Me he demorao ", fin-ini)
-
+        print("|| El mapa ha tardado: ", fin-ini)
+        ini2 = time()
         self.comparacionEnMapa()
+        fin2 = time()
+        print("| Calculando me he tardado: ", fin2-ini2)
 
     def comparacionSimple(self):
-        for i in range(len(diccionarioAbejitas)):
-            for j in range(i + 1, len(diccionarioAbejitas)):
-                self.calcularDistancia(diccionarioAbejitas.get(i),diccionarioAbejitas.get(j))
+        for i in range(len(listaAbejitas)):
+            for j in range(i + 1, len(listaAbejitas)):
+                self.calcularDistancia(listaAbejitas.get(i),listaAbejitas.get(j))
 
     def comparacionEnMapa(self):
         for i in range(len(self.mapa)):
             for j in range(len(self.mapa[i])):
                 for k in range(len(self.mapa[i][j])):
                     bee1 = self.mapa[i][j][k]
-                    if bee1.peligro == False:
+                    if bee1[3] == False:
                         for l in range(k+1, len(self.mapa[i][j])):
                             bee2 = self.mapa[i][j][l]
                             self.calcularDistancia(bee1, bee2)
@@ -122,22 +109,20 @@ class Colisiones:
 
 abejasAux = open("./datasets/ConjuntoDeDatosCon1000000abejas.txt").readlines()
 #creacion del diccionario
-diccionarioAbejitas = {}
+listaAbejitas = []
 for indice,abeja in enumerate(abejasAux):
     if indice == 0:
         continue
     abeja = abeja.split(",")
-    abejita = Abeja(float(abeja[1]),float(abeja[0]),float(abeja[2]),indice-1)
-    diccionarioAbejitas[indice-1] = abejita
+    listaAbejitas.append([float(abeja[1]), float(abeja[0]), float(abeja[2]), False])
 #creacion de la matriz
 mapa = []
 mapGrid = 16
-if len(diccionarioAbejitas) > 10000:
+if len(listaAbejitas) > 10000:
     mapGrid = 64
 for i in range(0,mapGrid):
     mapa.append([])
     for j in range (0,mapGrid):
         mapa[i].append([])
-
 abejasAux = None
-analizador = Colisiones(diccionarioAbejitas,mapGrid)
+analizador = Colisiones(listaAbejitas,mapGrid)
